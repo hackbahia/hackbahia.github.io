5bb5252d6618302123baf4a6939adb5e061f27e8
hackbahia <hck.ba@protonmail.ch>
2022-06-01 13:54:28 -0300

    .

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 8, 27, 9)}); // year, month, date, hour
+        });
+});		
+
