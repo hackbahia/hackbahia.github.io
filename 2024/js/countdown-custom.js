83cc7bbe08046f06be0e0519f6a73480b0f1f649
hackbahia <hck.ba@protonmail.ch>
2024-04-15 00:50:31 -0300

    .

---
+++ 2024/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 7, 26, 9)}); // year, month, date, hour
+        });
+});		
+
--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 7, 26, 9)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 7, 24, 8)}); // year, month, date, hour
         });
 });		
 
