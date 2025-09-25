a319e4eb2e555193059c5bb92616eca392df036b
hackbahia <hck.ba@protonmail.ch>
2025-09-25 00:51:22 -0300

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+Site
+
+https://hackbahia.github.io
---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2025, 9, 18, 8)}); // year, month, date, hour
+        });
+});		
+
